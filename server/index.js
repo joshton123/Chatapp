1bdf01e2bb0dc67699703780d6c960778417b851
joshton123 <124593317+joshton123@users.noreply.github.com>
2024-04-29 20:11:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const server = app.listen(process.env.PORT , ()=>{
 })
 
 const io = new Server(server, {
-    cors: { origin: "http://localhost:3000" },
+    cors: { origin: "https://chatapp-smoky-chi.vercel.app" },
   });
 
 global.onlineUsers = new Map();
